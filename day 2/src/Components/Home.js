c7ea7a4fd4bc4f25e3bc40d7061572bbfc64094b
Makilesh <125253325+Makilesh@users.noreply.github.com>
2023-11-08 09:14:49 +0530

    Add files via upload

---
+++ day 2/src/Components/Home.js
@@ -0,0 +1,46 @@
+import './Assets/style.css';
+export const Home = () => {
+    return (
+        <>
+        <body>
+
+      <form>
+        <center>
+            <h1>
+                Registration Form
+            </h1>
+            
+            <table>
+            <tr>
+                <td>
+
+            import { DiAndroid} from "react-icons/di";
+                </td>
+                {/* <
+                <td>Name</td>
+                <td><input type="text" placeholder="Name" /></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="text" placeholder="Password" /></td>
+            </tr>
+            <tr>
+                <td>Email ID</td>
+                <td><input type="text" placeholder="Email ID" /></td>
+            </tr>
+
+            <tr style ={{color:'green'}}>
+                Not a Robot
+                <td><input type="checkbox"  /></td> */}
+            </tr>
+            
+                <h1 style ={{color:'cyan', backgroundColor:'red'}}></h1>
+                
+            </table>
+        </center>   
+        </form>
+        </body>
+        </>
+    )
+    }
+// export default Home;
